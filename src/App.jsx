754f2bb0c84f31314f7145ba7026a2db99fa8bd1
Rafael Luiz <luizrafaelcontato@gmail.com>
2024-11-23 19:56:36 -0300

    Implementando o Taiwind e estilizando o titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-function App() {
-  return(
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-    </div>
-    
-  )
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+      </div>
+
+      <AddTask />
+      <Tasks />
+    </div>
+  );
 }
 
-export default App
+export default App;
