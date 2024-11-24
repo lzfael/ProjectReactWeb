67a5591cf8ea0c35c19e956f15b7171f648c8fdc
Rafael Luiz <luizrafaelcontato@gmail.com>
2024-11-23 21:54:46 -0300

    Criando o componete tasks e estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack.",
+      isComplated: true,
+    },
+    {
+      id: 2,
+      title: "Estudar ingles",
+      description: "Estudar ingles para se tornar fluente.",
+      isComplated: true,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar para melhorar minhas notas",
+      isComplated: false,
+    },
+  ]);
+
+  function onTaskClick(tasksId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isComplated: !task.isComplated };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
       </div>
-
       <AddTask />
-      <Tasks />
+      <Tasks tasks={tasks} onTaskClick={onTaskClick} />
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,23 @@
-function Tasks() {
-    return(
-        <div>
-            <h1>Tasks</h1>
-            
-        </div>
-    )
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onClick(task.id)}
+            className="bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md"
+          >
+            {task.title}
+            {task.isComplated ? "Complete" : "Imcomplete"}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
-export default Tasks
+export default Tasks;
