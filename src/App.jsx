79ede38aad48b76e51366c01a43517fe18eb9a40
Rafael Luiz <luizrafaelcontato@gmail.com>
2024-11-24 00:05:34 -0300

    Resolvendo Problema de Estilização

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,19 @@ function App() {
     },
     {
       id: 2,
-      title: "Estudar ingles",
+      title: "Estudar Ingles",
       description: "Estudar ingles para se tornar fluente.",
       isComplated: true,
     },
     {
       id: 3,
-      title: "Estudar matemática",
+      title: "Estudar Matemática",
       description: "Estudar para melhorar minhas notas",
       isComplated: false,
     },
   ]);
 
-  function onTaskClick(tasksId) {
+  function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       if (task.id === taskId) {
         return { ...task, isComplated: !task.isComplated };
@@ -37,13 +37,13 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px]  space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
-        </h1>
-      </div>
+        </h1> 
       <AddTask />
       <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -7,10 +7,9 @@ function Tasks(props) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => props.onClick(task.id)}
-            className="bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md"
-          >
+            className="bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md">
             {task.title}
-            {task.isComplated ? "Complete" : "Imcomplete"}
+            {task.isComplated ? "Complete" : "Imcompleto"}
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
