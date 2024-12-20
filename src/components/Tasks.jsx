a7fd4b733deef51e22e2d0a45b362e3cae5ecd2a
Rafael Luiz de Oliveira <luizrafaelcontato@gmail.com>
2024-12-20 20:05:26 -0300

    feat: implement task navigation to TaskPage

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,13 @@
 import { ChevronRightIcon, Trash2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({tasks, onTaskClick, onTaskDelete}) {
+  const navegate = useNavigate()
+  
+  function handleTaskClick(task){
+    navegate(`/task?title=${task.title}&description=${task.description}`) 
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -13,7 +20,9 @@ function Tasks({tasks, onTaskClick, onTaskDelete}) {
           > 
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button onClick={ () => {
+            handleTaskClick(task)
+          }} className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
           </button>
           <button 
