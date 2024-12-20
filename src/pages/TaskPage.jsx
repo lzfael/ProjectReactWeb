4cbb660663991cc11ed404442dfee0306d581939
Rafael Luiz de Oliveira <luizrafaelcontato@gmail.com>
2024-12-20 19:55:53 -0300

    feat: update TaskPage to display title and description from URL parameters

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,7 +1,15 @@
+import { useSearchParams } from "react-router-dom"
+
 export function TackPage(){
+    const [searchParams] = useSearchParams()
+    const title = searchParams.get("title")
+    const description = searchParams.get("description")
+
+
     return(
-        <>
-            <h1>Task Page</h1>
-        </>
+        <div className="h-screen w-screen bg-slate-500 p-6">
+            <h1>{title}</h1>
+            <h2>{description}</h2>
+        </div>
     )
 }
