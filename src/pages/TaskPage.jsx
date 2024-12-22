bf7d1fdafa44e9a89408e9ff74b2462c2293f45e
Rafael Luiz de Oliveira <luizrafaelcontato@gmail.com>
2024-12-22 00:04:48 -0300

    style: styling and organizing the TaskPage layout

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,15 +1,24 @@
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams } from "react-router-dom";
 
-export function TackPage(){
-    const [searchParams] = useSearchParams()
-    const title = searchParams.get("title")
-    const description = searchParams.get("description")
+export function TackPage() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
 
-
-    return(
-        <div className="h-screen w-screen bg-slate-500 p-6">
-            <h1>{title}</h1>
-            <h2>{description}</h2>
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6 flex flex-col items-center justify-start">
+      <div className="w-[500px] space-y-4 mt-6">
+        <div className="flex justify-center relative mb-6">
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
         </div>
-    )
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
