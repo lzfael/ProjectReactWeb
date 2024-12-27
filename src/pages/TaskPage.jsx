2f54164912c0385205529ae395f56a8979e5244b
Rafael Luiz de Oliveira <luizrafaelcontato@gmail.com>
2024-12-27 00:54:38 -0300

    refactor: creating a "Title" component to simplify the code

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon } from "lucide-react";
 import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Title from "../components/Title";
 
 export function TackPage() {
   const navigate = useNavigate();
@@ -18,10 +19,7 @@ export function TackPage() {
           >
             <ChevronLeftIcon />
           </button>
-
-          <h1 className="text-3xl text-slate-100 font-bold text-center">
-            Detalhes da Tarefa
-          </h1>
+          <Title>Detalhes da Tarefa</Title>
         </div>
 
         <div className="bg-slate-200 p-4 rounded-md">
